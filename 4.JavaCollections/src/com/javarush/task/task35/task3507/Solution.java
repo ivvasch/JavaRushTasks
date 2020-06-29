90ec91fe0e446fc83d03caaf09a5acc7765547dd
ivvasch <ivvasch@gmail.com>
2020-06-29 18:08:56 +0300

    получение класса с помощью ClassLoader

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -7,10 +7,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /* 
 ClassLoader - что это такое?
@@ -23,43 +20,44 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals){
-        // перебрать папку и записать все хранящиеся там файлы в list
-        // из листа методом рефлексии получить у каждого объект, проверить его на наличие конструктора и принадлежность к Animal
-        // поместить эти объекты в результирующий Set
-        // Создаем Set, который будем возвращать
-        Set<Animal> set = new TreeSet<>();
-        // создаем list для загрузки классов
+        /* перебрать папку с файлами,
+        методом рефлексии получить у каждого объект, проверить его на наличие конструктора и принадлежность к Animal,
+        поместить эти объекты в результирующий Set */
 
-        // считываем файлы в list
+        // Создаем Set, который будем возвращать
+        Set<Animal> set = new HashSet<>();
+        // перебираем файлы, для этого создаем объект File и передаем в него путь
         File dir = new File(pathToAnimals);
         for (File files: dir.listFiles()) {
             try {
+                // создаем отдельный класс MyClassLoader, создаем объект этого класса
                 MyClassLoader loader = new MyClassLoader();
-            Class<?> clazz = loader.load(files.toString());
-            Constructor<?> ctr = clazz.getConstructor();
-                if (Animal.class.isAssignableFrom(clazz)) {
+                // передаем имя класса в метод findClass класса MyClassLoader
+            Class<?> clazz = loader.findClass(files.toString());
+                // получаем публичный конструктор полученного класса
+            Constructor<?> ctr = (clazz.getConstructor());
+                // если конструктор есть, то у нас не выскакивает исключение и мы создаем объект этого класса, кастим его к Animal и добавляем его в set
                     set.add((Animal) ctr.newInstance());
-                }
-//            Class<?>[] inter = clazz.getInterfaces();
-//                for (Class<?> aClass : inter) {
-//                if (aClass.getSimpleName().equals("Animal"))
-//                    set.add((Animal) ctr.newInstance());
-//                }
-
+                // обрабатываем исключения
         } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
         return set;
     }
+    // создаем свой загрузчик классов и наследуемся от основного загрузчика
     public static class MyClassLoader extends ClassLoader {
-        public Class<?> load(String name) throws ClassNotFoundException {
+    // переопределяем метод и передаем в него полученный из нашей директории класс
+        public Class<?> findClass(String name) throws ClassNotFoundException {
+    // создаем массив байт для считывания в него файла
             byte[] bytes = new byte[0];
             try {
+    // считываем в него файл
                 bytes = Files.readAllBytes(Paths.get(name));
             } catch (IOException e) {
                 e.printStackTrace();
             }
+    // с помощью метода defineClass загружаем и возвращаем класс
             return defineClass(null, bytes, 0, bytes.length);
         }
     }
